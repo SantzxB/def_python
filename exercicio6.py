b087befdbaa45eec0c51154cc18e13420b6867b8
SantzxB <caiosantanaborges37@gmail.com>
2026-04-08 16:48:11 -0300

    Create exercicio6.py

---
+++ exercicio6.py
@@ -0,0 +1,13 @@
+import os
+
+os.system("cls||clear")
+
+def metros(a):
+    centimetros = a * 100
+    print(f"Centímetros: {centimetros}")
+    return a * 100
+
+n1 = int(input("Digite um valor que será transformado em centímetros: "))
+
+
+metros(n1)
